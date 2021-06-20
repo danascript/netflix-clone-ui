4addb73c9724526a9b33533a55cea3d0d33f948a
Daniela <daniela.fedyakin@gmail.com>
2021-06-20 19:12:59 +0200

    code along result

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import './normalize.css';
+
+import Navigation from './components/Navigation';
+import Billboard from './components/Billboard';
+import Titles from './components/Titles';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Billboard />
+            <Navigation />
+            <Titles />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
